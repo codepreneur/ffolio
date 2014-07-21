1cf6470eddabfac940cbeff7f98c6bfcde7747b7
gomenasaai <contramundee@gmail.com>
2014-07-21 11:35:51 +0100

    added angular yeoman scaffold

---
+++ ng-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ffolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ffolioApp
+ */
+angular.module('ffolioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
