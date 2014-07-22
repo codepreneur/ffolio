e9e1b8cf293e7cd12f5fe40c507263fbd73dccf7
gomenasaai <contramundee@gmail.com>
2014-07-22 13:51:46 +0100

    removed portfolio from profile branch for now

--- ng-app/app/scripts/controllers/portfolio.js
+++ ng-app/app/scripts/controllers/portfolio.js
@@ -2,54 +2,5 @@
 
 angular.module('ffolioApp')
 .controller('PortfolioCtrl',function($scope, $http, $location, $upload, $rootScope){
-    $scope.imageUploads = [];
-        $scope.abort = function(index) {
-            $scope.upload[index].abort();
-            $scope.upload[index] = null;
-        };
-
-        $scope.onFileSelect = function ($files) {
-            $scope.files = $files;
-            $scope.upload = [];
-            for (var i = 0; i < $files.length; i++) {
-                var file = $files[i];
-                file.progress = parseInt(0);
-                (function (file, i) {
-                    $http.get('/api/s3?mimeType='+ file.type).success(function(response) {
-                        var s3Params = response;
-                        $scope.upload[i] = $upload.upload({
-                            url: 'https://ffolio.s3.amazonaws.com/',
-                            method: 'POST',
-                            data: {
-                                'key' : 's3UploadExample/'+ Math.round(Math.random()*10000) + '$$' + file.name,
-                                'acl' : 'public-read',
-                                'Content-Type' : file.type,
-                                'AWSAccessKeyId': s3Params.key,
-                                'Policy' : s3Params.policy,
-                                'Signature' : s3Params.signature
-                            },
-                            file: file,
-                        }).then(function(response) {
-                            file.progress = parseInt(100);
-                            if (response.status === 201) {
-                                var data = xml2json.parser(response.data),
-                                parsedData;
-                                parsedData = {
-                                    location: data.postresponse.location,
-                                    bucket: data.postresponse.bucket,
-                                    key: data.postresponse.key,
-                                    etag: data.postresponse.etag
-                                };
-                                $scope.imageUploads.push(parsedData);
-
-                            } else {
-                                alert('Upload Failed');
-                            }
-                        }, null, function(evt) {
-                            file.progress =  parseInt(100.0 * evt.loaded / evt.total);
-                        });
-                    });
-                }(file, i));
-            }
-        };
+    
 });
